9c72bfee1506fec0220e7324e04973d0d5c55fb4
Mark <wancy86@sina.com>
2016-01-14 23:17:55 +0800

    sss

--- src/main/java/com/boystyle/controller/UserController.java
+++ src/main/java/com/boystyle/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.boystyle.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,9 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
 	
+	private static Logger log = LoggerFactory.getLogger(UserController.class);
+	
 	@RequestMapping("/list")
 	public String userlist(){
 		
+		log.debug("this is use lsit -------------------------------");
 		return "hello";
 	}
 	
